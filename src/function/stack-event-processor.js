1a0877d8970e7625b14c85fca60b5fa94947392f
Kedar Kolhatkar <kkolhatkar@collegeboard.org>
2020-12-19 01:36:44 -0500

    added sam

--- src/function/stack-event-processor.js
+++ src/function/stack-event-processor.js
@@ -1,6 +1,13 @@
-const handler = (event, context) => {
+async function waitFor() {
+  console.log('waitFor called');
+  return Promise.resolve('success');
+}
+
+const handler = async (event, context) => {
   console.log('Lambda function called');
   console.log(`event: ${JSON.stringify(event)}`);
+  await waitFor();
+  console.log('executing after waitFor');
 };
 
 export { handler };
